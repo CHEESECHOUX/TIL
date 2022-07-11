5911d9226bbdb4f3fc23727417b1f1e4446ef9c6
JISOO CHOI <gsoosuu@gmail.com>
2022-07-11 13:16:00 +0900

    Update 큰 수의 법칙 (P.92).py

--- 이코테/Greedy/큰 수의 법칙 (P.92).py
+++ 이코테/Greedy/큰 수의 법칙 (P.92).py
@@ -1,3 +1,5 @@
+##
+#M이 10,000이하일 경우 풀이법
 n, m, k = map(int, input().split())
 data = list(map(int, input().split()))
 
@@ -19,3 +21,21 @@ while True:
     m -= 1
 
 print(result)
+##
+#M의 크기가 100억 이상일 경우, 시간초과 되지 않는 풀이법
+n, m, k = map(int, input().split())
+data = list(map(int, input().split()))
+
+data.sort()
+first = data[n-1]
+second = data[n-2]
+
+count = int(m / (k+1)) * k
+count += m % (k+1)
+
+result = 0
+result += (count) * first
+result += (m - count) * second
+
+print(result)
+##
