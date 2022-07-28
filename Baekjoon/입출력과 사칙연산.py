acadfd291c2aa005b6e4aff13a4d50ea75af550e
JISOO CHOI <gsoosuu@gmail.com>
2022-07-28 19:28:38 +0900

    Create 입출력과 사칙연산.py

---
+++ Baekjoon/입출력과 사칙연산.py
@@ -0,0 +1,79 @@
+##
+#2557
+print("Hello World")
+
+#10718
+print("강한친구 대한육군\n강한친구 대한육군")
+
+#10171
+print("\    /\ \n )  ( ')\n(  /  )\n \(__)|")
+
+#10172
+print('|\_/|')
+print('|q p|   /}')
+print('( 0 )"""\\')
+print('|"^"`    |')
+print('||_/=\\\__|')
+
+#1000
+a, b = map(int, input().split())
+a > 0, b < 10
+print(a+b)
+
+#1001
+a, b = map(int, input().split())
+a > 0, b < 10
+print(a-b)
+
+#10998
+a, b = map(int, input().split())
+a > 0, b < 10
+print(a*b)
+
+#1008
+a, b = map(int, input().split())
+a > 0, b < 10
+print(a/b)
+
+#10869
+a, b = map(int, input().split())
+1 <= a, b <= 10000
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+
+#10926
+id = input()
+print(id + '??!')
+
+#18108
+year = int(input())
+print(year - 543)
+
+#10430
+a, b, c = map(int, input().split())
+
+print((a+b)%c)
+print(((a%c)+(b%c))%c)
+print((a*b)%c)
+print(((a%c)*(b%c))%c)
+
+#2588
+a = int(input())
+b = input() #문자열 형태로 받기
+
+print(a*int(b[2]))
+print(a*int(b[1]))
+print(a*int(b[0]))
+print(a*int(b))
+
+#25083
+print("""         ,r'"7""")
+print("""r`-_   ,'  ,/""")
+print(""" \. ". L_r'""")
+print("""   `~\/""")
+print("""      |""")
+print("""      |""")
+##
