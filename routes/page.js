9815cf5994db80f211bf6904388d76ee892fd667
cheesechoux <gsoosuu@gmail.com>
2022-08-01 17:21:14 +0900

    Create: Login_passport_module

--- routes/page.js
+++ routes/page.js
@@ -1,12 +1,11 @@
 const express = require('express');
 
 const router = express.Router();
-
 router.use((req, res, next) => {
-  res.locals.user = null;
+  res.locals.user = req.user;
   res.locals.followerCount = 0;
   res.locals.followingCount = 0;
-  res.locals.followerldList = [];
+  res.locals.followerIdList =[];
   next();
 });
 
