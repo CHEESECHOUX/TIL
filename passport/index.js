848ca739d1591c5bf1f27de78615237da0bfd270
cheesechoux <gsoosuu@gmail.com>
2022-08-01 20:50:01 +0900

    Create: Login_local

--- passport/index.js
+++ passport/index.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 const local = require('./localStrategy');
 const kakao = require('./kakaoStrategy');
-const User = requrie('..models/user');
+const User = require('../models/user');
 
 module.exports = () => {
   passport.serializeUser((user, done) => {
@@ -9,7 +9,7 @@ module.exports = () => {
   });
 
   passport.deserializeUser((id, done) => {
-    User.findOne({where: {id}})
+    User.findOne({ where: { id } })
       .then(user => done(null, user))
       .catch(err => done(err));
   });
