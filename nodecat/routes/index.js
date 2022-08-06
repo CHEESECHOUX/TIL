8c74242d4928d5cc065835ae31ebf85c7accfe78
cheesechoux <gsoosuu@gmail.com>
2022-08-06 16:50:24 +0900

    Create: express-rate-limit

--- nodecat/routes/index.js
+++ nodecat/routes/index.js
@@ -2,25 +2,25 @@ const express = require('express');
 const axios = require('axios');
 
 const router = express.Router();
-const URL = 'http://localhost:8002/v1';
+const URL = 'http://localhost:8002/v2';
 
 axios.defaults.headers.origin = 'http://localhost:4000'; // origin 헤더 추가
 const request = async (req, api) => {
   try {
     if (!req.session.jwt) { // 세션에 토큰이 없으면
-    const tokenResult = await axios.post(`${URL}/token`, {
-      clientSecret: process.env.CLIENT_SECRET,
-    });
-    req.session.jwt = tokenResult.data.token; // 재사용을 위해 세션에 토큰 저장
+      const tokenResult = await axios.post(`${URL}/token`, {
+        clientSecret: process.env.CLIENT_SECRET,
+      });
+      req.session.jwt = tokenResult.data.token; // 세션에 토큰 저장
     }
     return await axios.get(`${URL}${api}`, {
       headers: { authorization: req.session.jwt },
-    }); // API요청
+    }); // API 요청
   } catch (error) {
-    if (error.response.status === 419) { // 토큰 만료 시 토큰 재발급 받기
+    if (error.response.status === 419) { // 토큰 만료시 토큰 재발급 받기
       delete req.session.jwt;
       return request(req, api);
-    } // 419 외의 다른 에러라면
+    } // 419 외의 다른 에러면
     return error.response;
   }
 };
