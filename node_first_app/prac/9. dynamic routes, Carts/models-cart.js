af7ee6f17c77e9203a0444fef70b9dcfebfb0165
cheesechoux <gsoosuu@gmail.com>
2022-09-05 17:16:12 +0900

    Create: Cart Delete

--- node_first_app/prac/9. dynamic routes, Carts/models-cart.js
+++ node_first_app/prac/9. dynamic routes, Carts/models-cart.js
@@ -1,14 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 
-const p = path.join(
+const p = path.join( // 경로 구축
   path.dirname(process.mainModule.filename), 
   'data', 
   'cart.json'
 );
 
+// constructor가 아닌 static. 계속 다시 생성할 객체가 아닌, 애플리케이션 내에 하나의 장바구니이기 때문에 
 module.exports = class Cart { 
-  static addProduct(id, productPrice) {
+  static addProduct(id, productPrice) { // 추가하길 원하는 제품의 id // totalPrice 하기위해 productPrice 불러옴
     // 기존 또는 이전 cart를 파일에서 불러온 다음
     fs.readFile(p, (err, fileContent) => {
       let cart = { products: [], totalPrice: 0 };
@@ -17,13 +18,13 @@ module.exports = class Cart {
       }
     // 이를 분석해서 해당 제품이 이미 있는지 확인 => 기존 제품을 찾고
       const existingProductIndex = cart.products.findIndex(
-        prod => prod.id === id 
+        prod => prod.id === id  // 추가하려는 id와 같은지
       );
       const existingProduct = cart.products[existingProductIndex];
       let updatedProduct; 
       // 새로운 제품을 추가하거나 수량을 증가
-      if (existingProduct) {
-        updatedProduct = { ...existingProduct };
+      if (existingProduct) { // 기존 제품이 있다면 수량 증가
+        updatedProduct = { ...existingProduct }; // existingProduct 속성을 그대로 가져오기
         updatedProduct.qty = updatedProduct.qty + 1;
         cart.products = [...cart.products];
         cart.products[existingProductIndex] = updatedProduct;
@@ -40,16 +41,16 @@ module.exports = class Cart {
 
   static deleteProduct(id, productPrice) {
     fs.readFile(p, (err, fileContent) => {
-      if (err) { 
+      if (err) { // 장바구니를 찾지 못했다면 삭제할 제품이 없으니까 그대로 리턴
         return; 
       }
-      const updatedCart = { ...JSON.parse(fileContent) };
+      const updatedCart = { ...JSON.parse(fileContent) }; // 기존 장바구니의 모든 속성을 새로운 객체로 넣기
       const product = updatedCart.products.find(prod => prod.id === id);
       if (!product) {
-        return;
+        return; //제품이 없으면 그대로 리턴
       }
-      const productQty = product.qty;
-      updatedCart.products = updatedCart.products.filter(
+      const productQty = product.qty; // 수량 정보
+      updatedCart.products = updatedCart.products.filter( // 모델에서 했던 것과 똑같이 반환값이 true인 요소만 유지 (삭제 아닌 것)
       prod => prod.id !== id
       );
       updatedCart.totalPrice =
@@ -61,13 +62,13 @@ module.exports = class Cart {
     });
   }
 
-  static getCart(cb) {
+  static getCart(cb) { // 제품 정보를 받은 후 호출하는 콜백
     fs.readFile(p, (err, fileContent) => {
       const cart = JSON.parse(fileContent);
       if (err) {
         cb(null);
       } else {
-        cb(cart);
+        cb(cart); // 콜백에서 장바구니 전체를 반환
       }
     });
   }
--- node_first_app/prac/9. dynamic routes, Carts/models-product.js
+++ node_first_app/prac/9. dynamic routes, Carts/models-product.js
@@ -1,7 +1,7 @@
 const fs = require('fs'); 
 const path = require('path');
 
-const Cart = require('./cart');
+const Cart = require('./models-cart');
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
@@ -40,7 +40,7 @@ module.exports = class Product {
           console.log(err);
         });
       } else {
-        this.id = Math.random().toString();
+        this.id = Math.random().toString(); // product id 만들어주기 (위에 export할 때 만들어줘도 되고 save 할 때 만들어줘도 됨)
         products.push(this);
         fs.writeFile(p, JSON.stringify(products), err => {
           console.log(err);
@@ -49,11 +49,11 @@ module.exports = class Product {
     });
   }
 
-  static deleteById(id) {
+  static deleteById(id) { // 여기서 제품을 삭제하고 장바구니에도 제품이 있다면 삭제 (cart model에서 처리)
     getProductsFromFile(products => {
-      const product = products.find(prod => prod.id === id);
-      const updatedProducts = products.filter(prod => prod.id !== id);
-      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+      const product = products.find(prod => prod.id === id); // 제품의 가격을 알기 위해 제거하기 전에 제품 추출
+      const updatedProducts = products.filter(prod => prod.id !== id); // 삭제하려는 id가 아닌 값을 가진 id들을 새로운 배열로 만들기
+      fs.writeFile(p, JSON.stringify(updatedProducts), err => { // 콜백 함수로 에러 처리
         if (!err) {
           Cart.deleteProduct(id, product.price);
         }
@@ -65,10 +65,10 @@ module.exports = class Product {
     getProductsFromFile(cb);
   }
 
-  static findById(id, cb) {
-    getProductsFromFile(products => {
-      const product = products.find(p => p.id === id); 
-      cb(product);
-    });
+  static findById(id, cb) { // 제품 검색을 마치면 실행할 콜백
+    getProductsFromFile(products => { // 모든 제품을 가져와 파일 전체를 읽고, 
+      const product = products.find(p => p.id === id); // 원하는 id를 가진 제품 하나를 필터 
+      cb(product);                                     // find 메서드로 product 검색. 전달하는 함수값이 true에 해당하는 원소를 회신. (짧은 화살표 구문)
+    });                                                // 현재보고있는 product id가 인수로 받은 id와 같은지 확인 
   }
 };
