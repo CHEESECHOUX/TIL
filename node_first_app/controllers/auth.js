d87ae6f51425a8681eeb23959d218818c7d20c15
cheesechoux <gsoosuu@gmail.com>
2022-09-27 14:33:31 +0900

    Create: loginCookie

--- node_first_app/controllers/auth.js
+++ node_first_app/controllers/auth.js
@@ -1,12 +1,18 @@
 exports.getLogin = (req, res, next) => {
+  // const isLoggedIn = req
+  //   .get('Cookie')
+  //   .split(';')[1]  // 두번째 쿠키 추출
+  //   .trim()         // 공백 없애기
+  //   .split('=')[1]; // true 값
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: req.isLoggedIn
+    // isAuthenticated: isLoggedIn
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  req.isLoggedIn = true;
+  res.setHeader('Set-Cookie', 'loggedIn=true; Max-Age=10');
   res.redirect('/');
 };
