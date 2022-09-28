e5817b144a92ab2f6f0f634a25c8ac78f86e9175
cheesechoux <gsoosuu@gmail.com>
2022-09-28 14:31:39 +0900

    Create: session

--- node_first_app/app.js
+++ node_first_app/app.js
@@ -2,6 +2,8 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
@@ -12,7 +14,16 @@ const CartItem = require('./models/cart-item');
 const Order = require('./models/order');
 const OrderItem = require('./models/order-item');
 
+const options = {
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'password',
+  database: 'node_complete'
+}
+
 const app = express();
+const store = new MySQLStore(options);
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -24,6 +35,12 @@ const { all } = require('./routes/admin');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'my secret', 
+  resave: false, // 모든 응답이 아닌 세션이 변경되었을 때만 저장
+  saveUninitialized: false,
+  store: store,
+})); 
 
 app.use((req, res, next) => {
   User.findByPk(1)
@@ -59,7 +76,10 @@ sequelize
     })
   .then(user => {
     if (!user) {
-      return User.create({ name: 'Jisoo', email: 'Jisoo@test.com' });
+      return User.create({ 
+        name: 'Jisoo', 
+        email: 'Jisoo@test.com' 
+      });
     }
     return Promise.resolve(user);
     return user;
--- node_first_app/controllers/auth.js
+++ node_first_app/controllers/auth.js
@@ -4,6 +4,7 @@ exports.getLogin = (req, res, next) => {
   //   .split(';')[1]  // 두번째 쿠키 추출
   //   .trim()         // 공백 없애기
   //   .split('=')[1]; // true 값
+  console.log(req.session.isLoggedIn);
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
@@ -13,6 +14,7 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true; Max-Age=10');
+  // res.setHeader('Set-Cookie', 'loggedIn=true; Max-Age=10'); // 쿠키 설정
+  req.session.isLoggedIn = true;
   res.redirect('/');
 };
