f4ce8168c44f28abc71704d8e502d2b10b5ae9b4
cheesechoux <gsoosuu@gmail.com>
2022-12-15 23:52:46 +0900

    Create: symbol2

---
+++ JavaScript_prac/JavaScript_ES6/Basic/symbol/symbol2.js
@@ -0,0 +1,19 @@
+// Symbol()
+
+// 1. 파라미터에 주석, 설명을 작성
+const sym = Symbol("주석, 설명");
+console.log(sym);
+
+
+// 2. 문자열로 연결
+const sym2 = Symbol("설명");
+console.log(sym2.toString() + "연결");
+
+
+// 3. Template 에 사용
+const sym3 = Symbol("주석, 설명");
+try {
+  `${sym3}`
+} catch {
+  console.log("`${sym} 불가`");
+};
