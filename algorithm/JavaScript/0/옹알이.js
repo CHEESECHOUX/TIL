40635331d0111686547c3cbfd73bfdf7caeffdeb
cheesechoux <gsoosuu@gmail.com>
2023-02-11 21:23:51 +0900

    Update: 옹알이.js

--- algorithm/JavaScript/0/옹알이.js
+++ algorithm/JavaScript/0/옹알이.js
@@ -1,23 +1,23 @@
 // 10초 초과
-function solution(babbling) {
-  const can = ['aya','ye','woo','ma'];
-  let count = 0;
+// function solution(babbling) {
+//   const can = ['aya','ye','woo','ma'];
+//   let count = 0;
   
-  for(let i = 0; i < babbling.length; i++){
-    let word = babbling[i];
+//   for(let i = 0; i < babbling.length; i++){
+//     let word = babbling[i];
       
-    for(let j = 0; j < can.length; j++){
-      if(word.includes(can[j].repeat(2))){
-        break;
-      }
-      word = word.split(can[j]).join(" ");
-    }
-    if(word.split(" ").join("").length === 0){
-      count += 1;
-    }
-  }
-  return count;
-}
+//     for(let j = 0; j < can.length; j++){
+//       if(word.includes(can[j].repeat(2))){
+//         break;
+//       }
+//       word = word.split(can[j]).join(" ");
+//     }
+//     if(word.split(" ").join("").length === 0){
+//       count += 1;
+//     }
+//   }
+//   return count;
+// }
 
 
 //
@@ -30,12 +30,15 @@ function solution(babbling) {
     word = babbling[i].toString();
 
     for(let j = 0; j < babblingArr.length; j++) {
-      word = word.replaceAll(babblingArr[j], ' ');    
+      word = word.replaceAll(babblingArr[j], ' ');
     }
 
     if( word.trim().length == 0) {
-      answer++;
+      count++;
     }
   }
   return count;
 }
+
+let result = solution(["aya", "yee", "u", "maa", "wyeoo"])
+console.log(result)
