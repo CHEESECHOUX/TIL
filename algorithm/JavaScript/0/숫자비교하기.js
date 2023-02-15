f775f5f3e4e439e8443e6c86adfd3324662a95e3
cheesechoux <gsoosuu@gmail.com>
2023-02-15 15:06:01 +0900

    Create: 숫자비교하기

---
+++ algorithm/JavaScript/0/숫자비교하기.js
@@ -0,0 +1,15 @@
+//
+function solution(num1, num2) {
+  if (num1 === num2) {
+      return 1
+  } else {
+      return -1
+  }
+}
+
+
+//
+function solution(num1, num2) {
+  let answer = num1 === num2 ? 1 : -1;
+  return answer;
+}
