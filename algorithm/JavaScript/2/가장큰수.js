703a2e7e1ab515cbc2b0a20b862aaf3af89f6271
cheesechoux <gsoosuu@gmail.com>
2023-02-18 13:17:26 +0900

    Create: 가장큰수

---
+++ algorithm/JavaScript/2/가장큰수.js
@@ -0,0 +1,25 @@
+// 정렬
+// 아.. 몰게따
+// 한 자리 수면 10씩 곱해줘서 sort 해서 붙이려니까... 10 곱해져있쟈나..
+function solution2(numbers) {
+  let answer = '';
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] < 10) {
+      numbers[i] * 10 // 여기에 10도 안 곱해짐 모르겠슴
+    }
+  }
+  let numbersSort = numbers.sort();
+
+  return answer;
+}
+
+
+// 정답
+function solution(numbers) {
+  let answer = numbers.map(num => num + ''). // 각 숫자들을 문자로 변환
+                sort((a, b) => (b + a) - (a + b)).join(); // 문자로 변환된 숫자를 연결해 비교정렬해 정렬이 완료되면 join으로 다 붙여서 문자열 만들어주기
+
+  return answer[0] === '0' ? '0' : answer; // numbers 배열이 [0, 0, 0, 0]으로만 구성되어 있을 경우 '0'만 출력
+}
+
+console.log(solution([6, 10, 2]));
