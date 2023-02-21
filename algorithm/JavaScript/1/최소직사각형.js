2f5c3d574de93daefea4e32e8d93bd75f465e291
cheesechoux <gsoosuu@gmail.com>
2023-02-21 21:55:58 +0900

    Create: 최소직사각형

---
+++ algorithm/JavaScript/1/최소직사각형.js
@@ -0,0 +1,16 @@
+// 완전탐색
+function solution(sizes) {
+  let answer = [];
+  for (let i = 0; i < sizes.length; i++) {
+    console.log(sizes[i])
+    if (sizes[i][0] < sizes[i][1]) {
+      answer.push()
+    }
+  }
+  return answer
+}
+
+console.log(solution([[60, 50], [30, 70], [60, 30], [80, 40]]))
+
+
+//
--- algorithm/JavaScript/2/가장큰수.js
+++ algorithm/JavaScript/2/가장큰수.js
@@ -5,21 +5,23 @@ function solution2(numbers) {
   let answer = '';
   for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] < 10) {
-      numbers[i] * 10 // 여기에 10도 안 곱해짐 모르겠슴
+      let num = numbers[i] * 10
+      console.log(num)
     }
   }
-  let numbersSort = numbers.sort();
+  let numSort = numbers.sort();
 
   return answer;
 }
+solution2([6,10,2])
 
 
 // 정답
 function solution(numbers) {
   let answer = numbers.map(num => num + ''). // 각 숫자들을 문자로 변환
-                sort((a, b) => (b + a) - (a + b)).join(); // 문자로 변환된 숫자를 연결해 비교정렬해 정렬이 완료되면 join으로 다 붙여서 문자열 만들어주기
+                sort((a, b) => (b + a) - (a + b)).join(''); // 문자로 변환된 숫자를 연결해 비교정렬해 정렬이 완료되면 join으로 다 붙여서 문자열 만들어주기
 
   return answer[0] === '0' ? '0' : answer; // numbers 배열이 [0, 0, 0, 0]으로만 구성되어 있을 경우 '0'만 출력
 }
 
-console.log(solution([6, 10, 2]));
+solution([6, 10, 2]);
