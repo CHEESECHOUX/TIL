183f00ebf88f9db400119f8ea1427bb885745b41
cheesechoux <gsoosuu@gmail.com>
2023-02-23 14:21:09 +0900

    Update: n의배수고르기

--- algorithm/JavaScript/0/n의배수고르기.js
+++ algorithm/JavaScript/0/n의배수고르기.js
@@ -1,1 +1,14 @@
-let solution = (n, numlist) => numlist.filter(a => a%n === 0);
+function solution(n, numlist) {
+  let result = [];
+  for (let i = 0; i < numlist.length; i++) {
+    if (numlist[i] % n === 0) {
+      result.push(numlist[i])
+    }
+  }
+  return result;
+}
+
+console.log(solution(3, [4, 5, 6, 7, 8, 9, 10, 11, 12]))
+
+// 다른 사람 풀이
+let solution = (n, numlist) => numlist.filter(a => a % n === 0);
