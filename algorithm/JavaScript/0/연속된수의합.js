23384eb74048e4cd5b265f32918e812f7c5319ab
cheesechoux <gsoosuu@gmail.com>
2023-04-05 23:46:38 +0900

    Create: 종이자르기

--- algorithm/JavaScript/0/연속된수의합.js
+++ algorithm/JavaScript/0/연속된수의합.js
@@ -31,11 +31,12 @@ function solution(num, total) {
 
 
 //
-function solution(num, total) {
-  const min = Math.ceil(total / num - Math.floor(num / 2));
-  return Array.from({ length: num }, (_, i) => i + min);
-}
-// console.log(solution(3, 12)); // [3, 4, 5]
+// function solution(num, total) {
+//   const min = Math.ceil(total / num - Math.floor(num / 2));
+//   return Array.from({ length: num }, (_, i) => i + min);
+// }
+
+console.log(solution(3, 12)); // [3, 4, 5]
 // console.log(solution(5, 15)); // [1, 2, 3, 4, 5]
 // console.log(solution(4, 14)); // [2, 3, 4, 5]
 // console.log(solution(5, 5)); // [-1, 0, 1, 2, 3]
---
+++ algorithm/JavaScript/0/종이자르기.js
@@ -0,0 +1,13 @@
+// 첫번째 시도
+// function solution(M, N) {
+//   let answer = (M-1)+(N-1)*2
+//   return answer;
+// }
+
+//
+function solution(M, N) {
+  let answer = M * N - 1;
+  return answer;
+}
+
+console.log(solution(3, 2));
