e4b0263e9c3a3f2e40a513f61f77574ba2fd6b55
cheesechoux <gsoosuu@gmail.com>
2023-06-19 12:58:14 +0900

    단어정렬2

--- algorithm/JavaScript/BAEKJOON/정렬/단어정렬/1181.js
+++ algorithm/JavaScript/BAEKJOON/정렬/단어정렬/1181.js
@@ -7,6 +7,8 @@ n개의 단어
 const fs = require("fs");
 let input = fs.readFileSync("input.txt").toString().trim().split("\n");
 
+//
+// 첫 번째 풀이
 const n = Number(input.shift());
 
 input.sort((a, b) => {
@@ -14,5 +16,28 @@ input.sort((a, b) => {
 });
 
 const set = new Set(input);
-console.log(set);
-console.log(Array.from(set).join("\n")); // 객체를 배열로 변환
+console.log(Array.from(set).join("\n")); // set객체를 배열로 변환
+
+//
+// 두 번째 풀이
+const N = Number(input.shift());
+const sorted = [];
+const lengthArr = input.map((str) => str.length);
+const max = Math.max.apply(null, lengthArr);
+const min = Math.min.apply(null, lengthArr);
+
+for (let i = min; i <= max; i++) {
+  const group = [];
+  for (let j = 0; j < N; j++) {
+    if (input[j].length === i) {
+      // input배열의 j번재 요소의 문자열 길이가 i와 같다면
+      if (group.indexOf(input[j]) === -1) group.push(input[j]); // group 배열에 input[j]가 존재하지 않는다면, group 배열에 input[j] 추가
+    }
+  }
+  if (group.length > 1) {
+    sorted.push(...group.sort()); // group 배열이 2개 이상 요소를 갖는 경우, 배열 정렬
+  } else {
+    sorted.push(...group); // 아니면 그냥 추가
+  }
+}
+console.log(sorted.join("\n"));
