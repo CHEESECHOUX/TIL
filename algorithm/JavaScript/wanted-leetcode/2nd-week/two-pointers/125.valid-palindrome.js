6b2d216449dcb5b1a8a1ac292214cc71afb33fb9
cheesechoux <gsoosuu@gmail.com>
2023-08-25 22:27:51 +0900

    two-pointers

---
+++ algorithm/JavaScript/wanted-leetcode/2nd-week/two-pointers/125.valid-palindrome.js
@@ -0,0 +1,38 @@
+/*
+// 접근 방법
+1. 영문자가 아닌 문자를 모두 제거
+2. 모두 소문자로 변환
+
+3. 앞 뒤로 동일하게 읽히는 경우 true / 아니면 false
+투포인터로 중간에서 만나면 true
+*/
+
+let isPalindrome = function (s) {
+  s = s.toLowerCase(); // 대문자를 소문자로 먼저 변경해야함 // O(n)
+  s = s.replace(/[^a-z]/g, ""); // 소문자 외에는 모두 다 삭제시키니까 // O(n)
+
+  left = 0;
+  right = s.length - 1;
+
+  while (left < right) {
+    // O(n/2)
+    if (s[left] == s[right]) {
+      left++;
+      right--;
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+console.log(isPalindrome("race a car"));
+
+/*
+- Runtime : 57ms
+- 시간복잡도 : 
+
+two-pointers 예제를 보고 풀었는데
+확실히 속도가 빠르다... 이래서 사용하는 구나
+*/
